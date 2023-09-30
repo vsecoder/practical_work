5dfdfbeeee3129075e63d309dd4921bedde92072
Всеволод <kreepmeister@gmail.com>
2023-09-30 08:18:22 +0300

    Правки 5го задания

--- 3-4/Source5.cpp
+++ 3-4/Source5.cpp
@@ -7,13 +7,18 @@ int main()
     setlocale(LC_ALL, "Russian");
     // 5
 
-    int M, N, result; // переменные M и N и result
+    double M, N, result; // переменные M и N и result
 
     cout << "Введите значение M: ";
     cin >> M;
 
     cout << "Введите значение N: ";
     cin >> N;
+
+    if (N < 0 || M < 0) {
+        cout << "Ошибка: инвалид(,) данные" << endl;
+        return 0; // выход из программы
+    }
 
     // Проверяем деление на ноль
     if (N == 0)
