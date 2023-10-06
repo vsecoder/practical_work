469ce0c06464bbbfa0f08cd1bb11a39e47a74cf9
Всеволод <kreepmeister@gmail.com>
2023-10-06 12:24:10 +0300

    Правки по рекомендациям

--- 3-4/Source2.cpp
+++ 3-4/Source2.cpp
@@ -8,7 +8,7 @@ int main()
     setlocale(LC_ALL, "Russian");
 
     // 2
-    double AB, BC;       // длины сторон AB и BC
+    double AB, BC, AC;   // длины сторон AB и BC и AC
     double angle_C = 90; // угол C в радианах
 
     cout << "Введите длину стороны AB: ";
@@ -23,17 +23,22 @@ int main()
         return 0;
     }
 
+    AC = sqrt(pow(AB, 2) - pow(BC, 2));
+    double cos_A = AC / BC;
+    double tan_A = AB / AC;
+
+    // Другой способ, при углу не 90
     // Переводим угол C из градусов в радианы
-    double angle_C_rad = angle_C * M_PI / 180.0;
+    //double angle_C_rad = angle_C * M_PI / 180.0;
 
     // Вычисляем длину стороны AC по теореме косинусов
-    double AC = sqrt(pow(AB, 2) + pow(BC, 2) - 2 * AB * BC * cos(angle_C_rad));
+    //double AC = sqrt(pow(AB, 2) + pow(BC, 2) - 2 * AB * BC * cos(angle_C_rad));
 
     // Вычисляем косинус угла A по определению косинуса
-    double cos_A = (pow(AB, 2) + pow(AC, 2) - pow(BC, 2)) / (2 * AB * AC);
+    //double cos_A = (pow(AB, 2) + pow(AC, 2) - pow(BC, 2)) / (2 * AB * AC);
 
     // Вычисляем тангенс угла A по определению тангенса
-    double tan_A = sin(angle_C_rad) / (AB / AC + cos(angle_C_rad));
+    //double tan_A = sin(angle_C_rad) / (AB / AC + cos(angle_C_rad));
 
     cout << "Косинус угла A: " << cos_A << endl;
     cout << "Тангенс угла A: " << tan_A << endl;
