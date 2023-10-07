a4e5f95821c48d76c7ecd665646c3672b28afa2a
Всеволод <kreepmeister@gmail.com>
2023-10-07 09:04:28 +0300

    Второе задание

---
+++ 5-6/Source2.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    // 2
+
+    double a, b, discriminant, x1, x2;
+
+    cout << "Введите значение a: ";
+    cin >> a;
+    cout << "Введите значение b: ";
+    cin >> b;
+
+    discriminant = b * b - 4 * a;
+
+    if (discriminant > 0) {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Уравнение имеет два корня: x1 = " << x1 << ", x2 = " << x2 << endl;
+    }
+    else if (discriminant == 0) {
+        x1 = -b / (2 * a);
+        cout << "Уравнение имеет один корень: x = " << x1 << endl;
+    }
+    else {
+        cout << "Уравнение не имеет корней в действительной плоскости" << endl;
+    }
+
+    return 0;
+}
