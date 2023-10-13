e0f7dd6e0f0abb9e638361ff6646d14762dbf9da
Всеволод <kreepmeister@gmail.com>
2023-10-13 17:29:31 +0300

    Задача на циклы

---
+++ 7-8/Source2.cpp
@@ -0,0 +1,86 @@
+using namespace std;
+#include <iostream>
+
+string rubles(int cost)
+{
+    int remainder = cost % 10;
+
+    if ((cost % 100) >= 10 && (cost % 100) <= 20)
+    {
+        return "рубль";
+    }
+    else
+    {
+        switch (remainder)
+        {
+        case 1:
+            return "рубль";
+        case 2:
+        case 3:
+        case 4:
+            return "рубля";
+        default:
+            return "рублей";
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    // 2
+    int cost, type, count;
+    string r;
+    string hr = "-----------";
+
+    cout << "Введите цену товара: ";
+    cin >> cost;
+
+    cout << "Введите тип функции (f - цикл for, w - цикл while, d – do-while.): ";
+    cin >> type;
+
+    cout << "Введите количество товара: ";
+    cin >> count;
+
+    r = rubles(cost);
+
+    switch (type)
+    {
+    case 'f':
+        for (int i = 0; i < count; i++)
+        {
+            cout << hr << endl;
+            cout << "Ручка шариковая" << endl;
+            cout << "Шт." << cost << " " << r << endl;
+        }
+        cout << hr << endl;
+        break;
+
+    case 'w':
+        while (count > 0)
+        {
+            cout << hr << endl;
+            cout << "Ручка шариковая" << endl;
+            cout << "Шт." << cost << " " << r << endl;
+            count--;
+        }
+        cout << hr << endl;
+        break;
+
+    case 'd':
+        do
+        {
+            cout << hr << endl;
+            cout << "Ручка шариковая" << endl;
+            cout << "Шт." << cost << " " << r << endl;
+            count--;
+        } while (count > 0);
+        cout << hr << endl;
+        break;
+
+    default:
+        break;
+    }
+
+    return 0;
+}
