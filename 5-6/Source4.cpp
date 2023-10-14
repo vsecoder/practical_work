e8af3fb8f0320e680825ca244f37da763b57c28c
Всеволод <kreepmeister@gmail.com>
2023-10-14 08:20:20 +0300

    Правка под условие

--- 5-6/Source4.cpp
+++ 5-6/Source4.cpp
@@ -8,17 +8,22 @@ int main()
     // 4
 
     int n, count, discount;
-    int products[4];
+    int products_price[4];
+    string product_name[4];
 
     cout << "Выберите товар: " << endl;
     cout << "№1 - Худи 1200р" << endl;
-    products[0] = 1200;
+    products_price[0] = 1200;
+    product_name[0] = "худи";
     cout << "№2 - Джинсы 1700р" << endl;
-    products[1] = 1700;
+    products_price[1] = 1700;
+    product_name[1] = "джинсы";
     cout << "№3 - Кеды 3000р" << endl;
-    products[2] = 3000;
+    products_price[2] = 3000;
+    product_name[2] = "кеды";
     cout << "№4 - Носки 270р" << endl;
-    products[3] = 270;
+    products_price[3] = 270;
+    product_name[3] = "носки";
     cout << "Ввежите номер товара (1-4): " << endl;
 
     cin >> n;
@@ -33,10 +38,10 @@ int main()
     cin >> count;
     count = abs(count);
 
-    if (products[n] * count < 500) {
+    if (products_price[n] * count < 500) {
         discount = 0;
     }
-    else if (products[n] * count > 500 && products[n] * count < 1000) {
+    else if (products_price[n] * count > 500 && products_price[n] * count < 1000) {
         discount = 20;
     }
     else {
@@ -45,11 +50,11 @@ int main()
 
     cout << "----------" << endl;
     cout << "Ваш чек: " << endl;
-    cout << "Товар №" << n+1 << " по цене " << products[n] << "р." << endl;
+    cout << "Товар " << product_name[n] << " по цене " << products_price[n] << "р." << endl;
     cout << "В количестве " << count << "." << endl;
     cout << "Скидка: " << discount << "%" << endl;
-    cout << "К оплате без скидки: " << products[n] * count << endl;
-    cout << "Итогово: " << products[n] * count - (products[n] * count / 100 * discount) << endl;
+    cout << "К оплате без скидки: " << products_price[n] * count << endl;
+    cout << "Итогово: " << products_price[n] * count - (products_price[n] * count / 100 * discount) << endl;
     cout << "----------" << endl;
 
     return 0;
