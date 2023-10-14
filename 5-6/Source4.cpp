f1966fb9c6bd2c3184f6adb562af67d73c979d72
Всеволод <kreepmeister@gmail.com>
2023-10-14 08:57:44 +0300

    Массивы осудили

--- 5-6/Source4.cpp
+++ 5-6/Source4.cpp
@@ -1,61 +1,69 @@
-using namespace std;
 #include <iostream>
-#include <cmath>
-
+#include <math.h>
+using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
     // 4
-
-    int n, count, discount;
-    int products_price[4];
-    string product_name[4];
-
-    cout << "Выберите товар: " << endl;
-    cout << "№1 - Худи 1200р" << endl;
-    products_price[0] = 1200;
-    product_name[0] = "худи";
-    cout << "№2 - Джинсы 1700р" << endl;
-    products_price[1] = 1700;
-    product_name[1] = "джинсы";
-    cout << "№3 - Кеды 3000р" << endl;
-    products_price[2] = 3000;
-    product_name[2] = "кеды";
-    cout << "№4 - Носки 270р" << endl;
-    products_price[3] = 270;
-    product_name[3] = "носки";
-    cout << "Ввежите номер товара (1-4): " << endl;
-
-    cin >> n;
-    n = abs(n);
-    if (n < 1 || n > 4) {
-        cout << "Чувак, товара 4 -_-";
-        return 0;
-    }
-    n -= 1;
-
-    cout << "Ввежите кол-во товара: " << endl;
+    double count, sum = 0, discount;
+    int user_choice;
+    cout << "Выберите товар, который хотите купить: " << endl;
+    cout << "1 - Чай - 150 рублей" << endl;
+    cout << "2 - Кофе - 230 рублей" << endl;
+    cout << "3 - Минеральная вода - 50 рублей" << endl;
+    user_choice = abs(user_choice);
+    cin >> user_choice;
+    cout << "Выберите кол-во товара: " << endl;
     cin >> count;
     count = abs(count);
-
-    if (products_price[n] * count < 500) {
-        discount = 0;
+    switch (user_choice) {
+    case 1:
+    {
+        sum = count * 150;
+        break;
     }
-    else if (products_price[n] * count > 500 && products_price[n] * count < 1000) {
+    case 2:
+    {
+        sum = count * 230;
+        break;
+    }
+    case 3:
+    {
+        sum = count * 50;
+        break;
+    }
+    }
+    if (sum < 500) {
+        cout << "Ваша скидка составляет 0%" << endl;
+    }
+    else if (sum > 500 && sum < 1000) {
+        cout << "Ваша скидка составляет 20%" << endl;
         discount = 20;
     }
-    else {
+    else if (sum > 1000) {
+        cout << "Ваша скидка составляет 40%" << endl;
         discount = 40;
     }
+    switch (user_choice) {
+    case 1:
+        cout << "Чек на покупку товара: " << endl;
+        cout << "Чай - 150 рублей, в количестве " << count << endl;
+        cout << "Стоимость заказа составляет: " << sum << endl;
+        cout << "С учетом скидки стоимость заказа составлят: " << sum - (sum * (discount / 100)) << endl;
+        break;
 
-    cout << "----------" << endl;
-    cout << "Ваш чек: " << endl;
-    cout << "Товар " << product_name[n] << " по цене " << products_price[n] << "р." << endl;
-    cout << "В количестве " << count << "." << endl;
-    cout << "Скидка: " << discount << "%" << endl;
-    cout << "К оплате без скидки: " << products_price[n] * count << endl;
-    cout << "Итогово: " << products_price[n] * count - (products_price[n] * count / 100 * discount) << endl;
-    cout << "----------" << endl;
-
-    return 0;
+    case 2:
+        cout << "Чек на покупку товара: " << endl;
+        cout << "Кофе - 230 рублей, в количестве " << count << endl;
+        cout << "Стоимость заказа составляет: " << sum << endl;
+        cout << "С учетом скидки стоимость заказа составлят: " << sum - (sum * (discount / 100)) << endl;
+        break;
+    case 3:
+        cout << "Чек на покупку товара: " << endl;
+        cout << "Минеральная вода - 50 рублей, в количестве " << count << endl;
+        cout << "Стоимость заказа составляет: " << sum << endl;
+        cout << "С учетом скидки стоимость заказа составляет: " << sum - (sum * (discount / 100)) << endl;
+        break;
+    }
 }
+
