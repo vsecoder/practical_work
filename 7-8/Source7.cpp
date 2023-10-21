20f58f262d5ccdc3bf939e697f88f26534474f38
Всеволод <kreepmeister@gmail.com>
2023-10-21 08:36:36 +0300

    Убрал лишнюю переменную

--- 7-8/Source7.cpp
+++ 7-8/Source7.cpp
@@ -10,6 +10,8 @@ int main() {
     cout << "Введите значение точности (от 0.9 до 0.00001): ";
     cin >> f;
 
+    f = abs(f);
+
     if (f < 0.00001 || f > 0.9) {
         cout << "Точность должна быть в диапазоне от 0.00001 до 0.9." << endl;
         return 1;
@@ -18,21 +20,19 @@ int main() {
     double S = 1.0;
     double znak = -1.0;
     double slug = 1.0;
-    double n = 1;
     double z = 2;
+    double n = 0;
     double n2 = 0;
-    double n3 = 0;
     double save = 1.0;
 
     do {
         save = slug;
         znak = -znak;
-        z = z + n2;
+        z = z + n;
         n++;
-        n2++;
-        n3 += 2;
-        cout << "S += " << znak * n3 << " / " << z << " * " << pow(3, n2) << endl;
-        slug = (znak * n3) / (pow(3, n2) * z);
+        n2 += 2;
+        cout << "S += " << znak * n2 << " / " << z << " * " << pow(3, n) << endl;
+        slug = (znak * n2) / (pow(3, n) * z);
         S += slug;
     } while (save >= f);
 
