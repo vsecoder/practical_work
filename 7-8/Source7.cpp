8cd8c157ded47ad5c2421aebf50de9ef046e3e8f
Всеволод <kreepmeister@gmail.com>
2023-10-21 08:23:09 +0300

    Фикс

--- 7-8/Source7.cpp
+++ 7-8/Source7.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    // 7
 
-    double S = 0.0, first = 1.0, second = 1.0, third = 1, num;
+    double f;
+    cout << "Введите значение точности (от 0.9 до 0.00001): ";
+    cin >> f;
 
-    cout << "Введите последовательность (число): ";
-    cin >> num;
-
-    while (num != 0) {
-        S += first / (second + third);
-        first += 1;
-        second += second;
-        third *= 3;
-        num -= 1;
+    if (f < 0.00001 || f > 0.9) {
+        cout << "Точность должна быть в диапазоне от 0.00001 до 0.9." << endl;
+        return 1;
     }
 
-    cout << round(S * 1000) / 1000;
+    double S = 0.0;
+    double znak = -1.0;
+    double slug = 1.0;
+    double n = 1;
+    double z = 2;
+    double n2 = 0;
+    double n3 = 0;
+
+
+    while (slug >= f) {
+        S += slug;
+        znak = -znak;
+        z = z + n2;
+        n++;
+        n2++;
+        n3 += 2;
+        cout << "S += " << znak * n3 << " / " << z << " * " << pow(3, n2) << endl;
+        slug = (znak * n3) / (pow(3, n2) * z);
+    }
+
+    cout << "S = " << S << endl;
+
     return 0;
 }
