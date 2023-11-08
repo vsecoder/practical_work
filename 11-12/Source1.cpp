189624cbb84895affc485571c114d3cbb6e38465
Всеволод <kreepmeister@gmail.com>
2023-11-08 09:34:36 +0300

    Методы сортировки

---
+++ 11-12/Source1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+void printMas(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int* bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    return arr;
+}
+
+
+int* choiceSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i)
+    {
+        int smallestIndex = i;
+
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (arr[j] < arr[smallestIndex])
+                smallestIndex = j;
+        }
+
+        swap(arr[i], arr[smallestIndex]);
+    }
+    return arr;
+}
+
+
+int* insertSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+
+int main() {  
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    // 1
+    const int n = 5;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 101 + 1;
+    }
+
+    cout << "Ваш массив: ";
+    printMas(arr, n);
+
+    cout << "Сортировка пузырьком: ";
+    printMas(bubbleSort(arr, n), n);
+
+    cout << "Сортировка выбором: ";
+    printMas(choiceSort(arr, n), n);
+
+    cout << "Сортировка вставками: ";
+    printMas(insertSort(arr, n), n);
+
+    return 0;
+}
